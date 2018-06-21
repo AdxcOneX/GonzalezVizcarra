47f3cdd0e7ff7c648e28d470b2cc73267516bd28
LProtox <36269176+LProtox@users.noreply.github.com>
2018-06-21 16:58:31 -0500

    Add files via upload

--- Sources/ALUControl.v
+++ Sources/ALUControl.v
@@ -19,13 +19,20 @@ module ALUControl
 	output [3:0] ALUOperation
 
 );
-
+//Primeros 3 bits los define el usuario
+//Restantes serian el opcode o funct
+//R type
 localparam R_Type_AND    = 9'b111_100100;
 localparam R_Type_OR     = 9'b111_100101;
 localparam R_Type_NOR    = 9'b111_100111;
 localparam R_Type_ADD    = 9'b111_100000;
+localparam R_Type_SUB 	 = 9'b111_100010;
+localparam R_Type_SLL 	 = 9'b111_000000;
+localparam R_Type_SRL 	 = 9'b111_000010;
+//I Type
 localparam I_Type_ADDI   = 9'b100_xxxxxx;
 localparam I_Type_ORI    = 9'b101_xxxxxx;
+localparam I_Type_LUI 	 = 9'b110_xxxxxx;
 
 
 reg [3:0] ALUControlValues;
@@ -35,12 +42,18 @@ assign Selector = {ALUOp, ALUFunction};
 
 always@(Selector)begin
 	casex(Selector)
+		//R_Type
 		R_Type_AND:    ALUControlValues = 4'b0000;
 		R_Type_OR: 		ALUControlValues = 4'b0001;
 		R_Type_NOR:    ALUControlValues = 4'b0010;
 		R_Type_ADD:    ALUControlValues = 4'b0011;
-		I_Type_ADDI:	ALUControlValues = 4'b0011;
-		I_Type_ORI:		ALUControlValues = 4'b0001;
+		R_Type_SUB:	 	ALUControlValues = 4'b0100;
+		R_Type_SLL: 	ALUControlValues = 4'b0101;
+		R_Type_SRL:	 	ALUControlValues = 4'b0110;
+		//I_Type
+		I_Type_ADDI:	ALUControlValues = 4'b1110;
+		I_Type_ORI:		ALUControlValues = 4'b1101;
+		I_Type_LUI:		ALUControlValues = 4'b1100;
 		
 		default: ALUControlValues = 4'b1001;
 	endcase
